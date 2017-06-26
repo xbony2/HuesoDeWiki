79414fee4ecf389fe7c109c08e9fd59ab845aebe
Eric Schneider <xbony22@gmail.com>
2017-06-25 21:27:27 -0400

    Closes #6 and #7 (first sentence generator, slightly more advanced)

---
+++ src/main/java/xbony2/huesodewiki/FirstSentenceCreator.java
@@ -0,0 +1,28 @@
+package xbony2.huesodewiki;
+
+import static xbony2.huesodewiki.Utils.getModName;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import xbony2.huesodewiki.infobox.parameters.TypeParameter;
+
+public class FirstSentenceCreator {
+	public static String createFirstSentence(ItemStack itemstack){
+		String name = itemstack.getDisplayName();
+		String modName = getModName(itemstack);
+		String type = new TypeParameter().getParameterText(itemstack);
+		String linkFix = HuesoDeWiki.linkCorrections.get(modName); //is null if there isn't a change required.
+		
+		StringBuilder ret = new StringBuilder();
+		ret.append("The '''");
+		ret.append(name);
+		ret.append("''' ");
+		ret.append(name.endsWith("s") ? "are " : "is ");
+		ret.append(type.startsWith("a") || type.startsWith("e") || type.startsWith("i") || type.startsWith("o") || type.startsWith("u") ? "an " : "a ");//blerg
+		ret.append(type);
+		ret.append(" added by [[").append((linkFix != null ? linkFix + "|" : ""));
+		ret.append(modName);
+		ret.append("]].\n");
+		return ret.toString();
+	}
+}
--- src/main/java/xbony2/huesodewiki/PageCreator.java
+++ src/main/java/xbony2/huesodewiki/PageCreator.java
@@ -13,20 +13,15 @@ import xbony2.huesodewiki.recipe.RecipeCreator;
 
 public class PageCreator {
 	public static void createPage(ItemStack itemstack){
-		String name = itemstack.getDisplayName();
-		String modName = getModName(itemstack);
-		
-		String linkFix = HuesoDeWiki.linkCorrections.get(modName); //is null if there isn't a change required.
-		
 		//And now for the magic
 		StringBuilder page = new StringBuilder(InfoboxCreator.createInfobox(itemstack));
 		
 		page.append('\n');
-		page.append("The '''").append(name).append("''' is ").append((itemstack.getItem() instanceof ItemBlock ? "a block" : "an item")).append(" added by [[").append((linkFix != null ? linkFix + "|" : "")).append(modName).append("]].").append('\n');
+		page.append(FirstSentenceCreator.createFirstSentence(itemstack));
 		page.append(RecipeCreator.createRecipes(itemstack));
 		page.append('\n');
 		page.append('\n');
-		page.append("{{Navbox ").append(modName).append("}}").append('\n');
+		page.append("{{Navbox ").append(getModName(itemstack)).append("}}").append('\n');
 		page.append('\n');
 		page.append(CategoryCreator.createCategories(itemstack)).append('\n');
 		page.append('\n');
