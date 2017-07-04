40629d24cc75c651b8d7ac19a4446cb7d0cfb690
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2017-07-03 21:29:01 -0700

    Compact -> Compat

--- src/main/java/xbony2/huesodewiki/compact/tesla/TeslaCompact.java
+++
@@ -1,7 +0,0 @@
-package xbony2.huesodewiki.compact.tesla;
-
-public class TeslaCompact {
-	public static void preInit(){
-		//TODO
-	}
-}
---
+++ src/main/java/xbony2/huesodewiki/compat/tesla/TeslaCompat.java
@@ -0,0 +1,7 @@
+package xbony2.huesodewiki.compat.tesla;
+
+public class TeslaCompat {
+	public static void preInit(){
+		//TODO
+	}
+}
