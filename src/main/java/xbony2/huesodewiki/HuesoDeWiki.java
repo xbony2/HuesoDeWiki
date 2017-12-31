397775deaccb05721add2b83ac951081b6ff890e
Eric Schneider <xbony22@gmail.com>
2017-12-31 10:39:22 -0500

    Let's just do a new version here (TODO: automatic version stuff. But I'm too lazy to make my life easier)

--- src/main/java/xbony2/huesodewiki/HuesoDeWiki.java
+++ src/main/java/xbony2/huesodewiki/HuesoDeWiki.java
@@ -30,7 +30,7 @@ import xbony2.huesodewiki.recipe.RecipeCreator;
 @Mod(modid = HuesoDeWiki.MODID, version = HuesoDeWiki.VERSION, clientSideOnly = true)
 public class HuesoDeWiki {
 	public static final String MODID = "huesodewiki";
-	public static final String VERSION = "1.3.2a";
+	public static final String VERSION = "1.3.3a";
 	
 	public static KeyBinding copyPageKey;
 	private boolean isCopyPageKeyDown = false;
