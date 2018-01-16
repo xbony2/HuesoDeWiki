9f9a89dc8bd60f79376db6627546901cc523c259
elifoster <elifosterwy@gmail.com>
2018-01-15 17:10:05 -0800

    Resolve part of #33, add new registryname parameter

---
+++ src/main/java/xbony2/huesodewiki/infobox/parameters/RegistryNameParameter.java
@@ -0,0 +1,22 @@
+package xbony2.huesodewiki.infobox.parameters;
+
+import net.minecraft.item.ItemStack;
+import xbony2.huesodewiki.api.infobox.IInfoboxParameter;
+
+public class RegistryNameParameter implements IInfoboxParameter {
+
+	@Override
+	public boolean canAdd(ItemStack itemstack){
+		return itemstack.getItem().getRegistryName() != null;
+	}
+
+	@Override
+	public String getParameterName(){
+		return "registryname";
+	}
+
+	@Override
+	public String getParameterText(ItemStack itemstack){
+		return itemstack.getItem().getRegistryName().toString();
+	}
+}
