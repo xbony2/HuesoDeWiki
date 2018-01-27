ecb87c7818318374acc891c52a3534397aeec685
Eric Schneider <xbony2@users.noreply.github.com>
2018-01-27 09:12:20 -0500

    Update BaublesCompat.java

--- src/main/java/xbony2/huesodewiki/compat/baubles/BaublesCompat.java
+++ src/main/java/xbony2/huesodewiki/compat/baubles/BaublesCompat.java
@@ -1,6 +1,5 @@
 package xbony2.huesodewiki.compat.baubles;
 
-
 import baubles.api.cap.BaublesCapabilities;
 import xbony2.huesodewiki.api.category.BasicCategory;
 import xbony2.huesodewiki.category.CategoryCreator;
@@ -9,10 +8,8 @@ import xbony2.huesodewiki.infobox.parameters.TypeParameter;
 public class BaublesCompat {
 	
 	public static void preInit(){
-		CategoryCreator.categories.add(new BasicCategory("Baubles", 
-				(itemstack) -> itemstack.hasCapability(BaublesCapabilities.CAPABILITY_ITEM_BAUBLE, null)));
+		CategoryCreator.categories.add(new BasicCategory("Baubles", (itemstack) -> itemstack.hasCapability(BaublesCapabilities.CAPABILITY_ITEM_BAUBLE, null)));
 
 		TypeParameter.types.add(new BaubleType());
 	}
-	
 }
