32c3906304736b3e8cec8792c78fd7a8d99a9691
Eric Schneider <xbony2@users.noreply.github.com>
2018-01-27 21:31:11 -0500

    Update MiningSpeedParameter.java

--- src/main/java/xbony2/huesodewiki/infobox/parameters/MiningSpeedParameter.java
+++ src/main/java/xbony2/huesodewiki/infobox/parameters/MiningSpeedParameter.java
@@ -9,6 +9,7 @@ import xbony2.huesodewiki.api.infobox.IInfoboxParameter;
 import java.lang.reflect.Field;
 
 public class MiningSpeedParameter implements IInfoboxParameter {
+	
 	@Override
 	public boolean canAdd(ItemStack itemstack) {
 		return itemstack.getItem() instanceof ItemTool;
@@ -23,13 +24,15 @@ public class MiningSpeedParameter implements IInfoboxParameter {
 	public String getParameterText(ItemStack itemstack) {
 		try {
 			Field field = Utils.getField(ItemTool.class, "toolMaterial", "field_77862_b");
+			
 			if(field != null){
 				field.setAccessible(true);
 				return Utils.floatToString(((Item.ToolMaterial) field.get(itemstack.getItem())).getEfficiency());
 			}
-		} catch(IllegalArgumentException | IllegalAccessException e) {
+		}catch(IllegalArgumentException | IllegalAccessException e){
 			e.printStackTrace();
 		}
+		
 		return "?";
 	}
 }
