9309afb5973ec55d6c819d4a71fa94b5d7fef503
Eric Schneider <xbony2@users.noreply.github.com>
2018-02-27 17:33:12 -0500

    K I S S

--- src/main/java/xbony2/huesodewiki/prefix/CorrectTitlePrefix.java
+++ src/main/java/xbony2/huesodewiki/prefix/CorrectTitlePrefix.java
@@ -5,18 +5,14 @@ import org.apache.commons.lang3.StringUtils;
 import xbony2.huesodewiki.api.IPagePrefix;
 
 public class CorrectTitlePrefix implements IPagePrefix {
-	/**
-	 * As defined at <a href="https://en.wikipedia.org/wiki/Wikipedia:Naming_conventions_(technical_restrictions)#Forbidden_characters">Wikipedia:Naming conventions (technical restrictions)#Forbidden characters</a>.
-	 */
-	private static final char[] INVALID_CHARS = { '#', '<', '>', '[', ']', '|', '{', '}' };
-
+	
 	@Override
 	public boolean canAdd(ItemStack itemstack){
-		return StringUtils.containsAny(itemstack.getDisplayName(), INVALID_CHARS);
+		return StringUtils.containsAny(itemstack.getDisplayName(), {'#', '<', '>', '[', ']', '|', '{', '}'}); // see: https://en.wikipedia.org/wiki/Wikipedia:NCHASHTAG
 	}
 
 	@Override
 	public String getText(ItemStack itemstack){
-		return String.format("{{Correct title|%s}}", itemstack.getDisplayName());
+		return "{{Correct title|", itemstack.getDisplayName() + "}}");
 	}
 }
